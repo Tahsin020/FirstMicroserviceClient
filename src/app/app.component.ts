c57bc7c496fc7bd79ae9242644a532ffa9febc95
Thsn020 <Ithsn8@gmail.com>
2024-08-10 19:44:13 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,68 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'FirstMicroserviceClient';
+  todos: TodoModel[] = [];
+  categories: CategoryModel[] = [];
+  work: string = '';
+  category: string = '';
+
+  constructor(private httpClient: HttpClient) {
+    this.getAllTodos();
+    this.getAllCategories();
+  }
+
+  getAllTodos() {
+    this.httpClient
+      .get<TodoModel[]>('http://localhost:5000/api/todos/getall')
+      .subscribe((res) => {
+        this.todos = res;
+      });
+  }
+
+  saveTodo() {
+    this.httpClient
+      .get('http://localhost:5000/api/todos/create?work=' + this.work)
+      .subscribe(() => {
+        this.getAllTodos();
+      });
+  }
+
+  getAllCategories() {
+    this.httpClient
+      .get<CategoryModel[]>('http://localhost:5000/api/categories/getall')
+      .subscribe((res) => {
+        this.categories = res;
+      });
+  }
+
+  saveCategory() {
+    const data = {
+      name: this.category,
+    };
+    this.httpClient
+      .post('http://localhost:5000/api/categories/create', data)
+      .subscribe(() => {
+        this.getAllCategories();
+      });
+  }
+}
+
+export class TodoModel {
+  id: number = 0;
+  work: string = '';
+}
+
+export class CategoryModel {
+  id: number = 0;
+  name: string = '';
 }
